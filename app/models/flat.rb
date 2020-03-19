316ad397259c9f8597f39cf728eb461bef57b145
Marco Montis <marcmontis7@gmail.com>
2020-03-19 19:07:00 +0100

    add picture_url to flats

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,4 @@
 class Flat < ApplicationRecord
+  validates :name, :address, :description, presence: true
+  validates :price_per_night, :number_of_guests, numericality: { only_integer: true }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,8 @@ Flat.create!(
   address: '10 Clifton Gardens London W9 1DT',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 75,
-  number_of_guests: 3
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156'
 )
 
 Flat.create!(
@@ -18,7 +19,8 @@ Flat.create!(
   address: '5 Queensmill Road London SW6 6JP',
   description: 'Lovely warm comfortable and stylishly furnished house. Private bedroom and bathroom with shared living areas',
   price_per_night: 65,
-  number_of_guests: 2
+  number_of_guests: 2,
+  picture_url: 'https://images.unsplash.com/photo-1507089947368-19c1da9775ae'
 )
 
 Flat.create!(
@@ -26,7 +28,8 @@ Flat.create!(
   address: 'Euston Rd London N1C 4QPT',
   description: 'A mini apartment within the clock tower apartment at St Pancras Station, with its own en suite bathroom and kitchen and sitting area.',
   price_per_night: 110,
-  number_of_guests: 2
+  number_of_guests: 2,
+  picture_url: 'https://images.unsplash.com/photo-1464890100898-a385f744067f'
 )
 
 Flat.create!(
@@ -34,5 +37,6 @@ Flat.create!(
   address: '72 Covent Gardens London C2 3BZ',
   description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
   price_per_night: 68,
-  number_of_guests: 4
+  number_of_guests: 4,
+  picture_url: 'https://images.unsplash.com/photo-1501183638710-841dd1904471'
 )
